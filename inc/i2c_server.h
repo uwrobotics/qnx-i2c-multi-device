c29bc0a6da3704fdc8dc5f5cb5d04bf1e142c5c2
Yuchen Lin <y29lin@uwaterloo.ca>
2025-11-02 15:15:26 -0500

    first draft

--- inc/i2c_server.h
+++ inc/i2c_server.h
@@ -1,3 +1,56 @@
+#include <iostream>
+#include <sys/neutrino.h>
+#include <sys/dispatch.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MY_PULSE_CODE _PULSE_CODE_MINAVAIL
+
+struct my_msg {
+    int type;
+    char text[64];
+};
+
+int main() {
+    name_attach_t *attach;
+    my_msg msg;
+    int rcvid;
+
+    // Attach a name so clients can find us
+    attach = name_attach(NULL, "MyServer", 0);
+    if (attach == NULL) {
+        perror("name_attach");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server started, waiting for messages..." << std::endl;
+
+    while (true) {
+        rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
+        if (rcvid == -1) {
+            perror("MsgReceive");
+            continue;
+        }
+
+        if (rcvid == 0) {
+            // Handle pulse
+            std::cout << "Received a pulse (code " 
+                      << msg.type << ")" << std::endl;
+        } else {
+            // Handle message
+            std::cout << "Server received: " << msg.text << std::endl;
+
+            // Reply back to client
+            strcpy(msg.text, "Hello from Server!");
+            MsgReply(rcvid, 0, &msg, sizeof(msg));
+        }
+    }
+
+    name_detach(attach, 0);
+    return 0;
+}
+
 class I2CServer {
-    // Create unordered map; manage and register all the slave device, handle communication
+    
 };
--- inc/json_helper.h
+++ inc/json_helper.h
@@ -1,5 +1,67 @@
+#ifndef JSON_HELPER_H
+#define JSON_HELPER_H
+
 #include <sys/json.h>
+#include <stdint.h>
+#include <string>
+#include <stdlib.h>
 
-class JsonHelper {
+/**
+ * struct {
+ * 'ID': ,1/init; 2/message
+ * 'data':,}
+ * slave_id:
+ * Type: mem/direct (interrupt not supported)
+ * 
+ * size:
+ * int_array:
+ * )
+ */
 
+
+// user input: one unordered map entry (name and type and value)
+// class will count the nest
+// output json object
+
+
+ class JsonEncode {
+    public:
+        JsonEncode() {
+            enc = json_encoder_create();
+            json_encoder_start_object(enc, NULL);
+        }
+
+        ~JsonEncode() {
+            json_encoder_destroy(enc);
+        }
+
+        std::string get_string() {
+            if(buf.empty()) {
+                json_encoder_end_object(enc);
+                if(get_status()==JSON_ENCODER_OK) {
+                    snprintf(str, max_finfo_size, "JSON:%s\n", json_encoder_buffer(enc));
+                    buf = str;
+                }
+            }
+
+            return buf;
+        }
+
+    private:
+        json_encoder_t *enc;
+        char str[1024];
+        unsigned max_finfo_size = 1024;
+        std::string buf;
+        
+
+        json_encoder_error_t get_status() {
+            json_encoder_error_t status = json_encoder_get_status(enc);
+
+            if ( status != JSON_ENCODER_OK ) {
+                perror("Json Error");
+                return status;
+            }
+        }
 };
+
+#endif // JSON_HELPER_H
